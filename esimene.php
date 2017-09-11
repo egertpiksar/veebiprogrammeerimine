b67c58787228494c8c7afa4ca11b7f74966fda17
Egert Piksar <pikseger@greeny.cs.tlu.ee>
2017-09-11 13:49:11 +0300

    Esimene PHP fail, kus on vaid html

---
+++ esimene.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Egert Piksari veebiprogrameerimise asjad</title>
+</head>
+<body>
+	<h1>Egert Piksar</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+	<p>Sügisuvitalv.</p>
+</body>
+</html>

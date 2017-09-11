e025bc670e65fb7b0d47837f50a46e6718af1386
Egert Piksar <pikseger@greeny.cs.tlu.ee>
2017-09-11 14:42:40 +0300

    Lisatud esimene php osa

--- esimene.php
+++ esimene.php
@@ -8,5 +8,14 @@
 	<h1>Egert Piksar</h1>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
 	<p>Sügisuvitalv.</p>
+	<?php 
+	
+	echo "<p>Kõige esimene php sõnum</p>";
+	echo "<p>Täna on ";
+	echo date("d.m.Y");
+	echo ".</p>";
+	echo "<p>Lehe avamise hetkel oli kell  " .date ("H.i.s") .".</p>";
+	
+	?>
 </body>
 </html>
